c21becc7bf2f3d90b601c89e2e3744e50d69f255
Richa  Bharti <richa.bharti@cginfinity.com>
2026-07-09 22:06:48 +0530

    react classwork submission

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -1,21 +1,41 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import About from './components/About'
-import Skills from './components/Skills'
-import Education from './components/Education'
-import SocialLinks from './components/SocialLinks'
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function Increment() {
+    setCount((prev) => prev + 1);
+  }
+  function Decrement() {
+    setCount((prev) => prev - 1);
+  }
+
   return (
-    <>
-      <Navbar />
-      <main className="container">
-        <About />
-        <Skills />
-        <Education />
-        <SocialLinks />
-      </main>
-    </>
+    <div>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/navbar">Navbar</Link>
+      </nav>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Counter App</h1>
+              <h2>{count}</h2>
+              <button onClick={Increment}>Increment</button>
+              <button onClick={Decrement}>Decrement</button>
+            </div>
+          }
+        />
+        <Route path="/navbar" element={<Navbar name="My navbar" />} />
+      </Routes>
+    </div>
   )
 }
 
--- REACT/src/components/Navbar.jsx
+++ REACT/src/components/Navbar.jsx
@@ -1,14 +1,11 @@
-function Navbar() {
+import { Link } from 'react-router-dom';
+
+function Navbar(prop) {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">My Portfolio</div>
-      <ul className="navbar-links">
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#education">Education</a></li>
-        <li><a href="#social">Social</a></li>
-      </ul>
-    </nav>
+    <div>
+        <Link to="/test">Test</Link>
+        <h1>{prop.name}</h1>
+    </div>
   )
 }
 
